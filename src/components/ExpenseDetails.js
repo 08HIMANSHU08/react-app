65ab9aa2dd32a93949846ce17f7fbb25db31a7ad
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 18:41:29 +0530

    expense details

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,12 +1,14 @@
 
+import React from 'react';
+
 import './ExpenseItem.css';
 
 
 function ExpenseDetails(props) {
     return (
         <div className='expense-item__description'>
-            <p>{props.location}</p>
-            <p>{props.desc}</p>
+            <h2>{props.location}</h2>
+            <h2>{props.desc}</h2>
             <div className='expense-item__price'>Rs {props.price}</div>
         </div>
         )
