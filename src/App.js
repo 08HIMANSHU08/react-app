c154dbda982bf10635d54aeb73cd752f7ce3b8b9
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 17:07:45 +0530

    app.js using map

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {location:"lucknow",price:50,desc:"Food"},
+    {location:"Kanpur",price:100,desc:"Movie"},
+    {location:"Prayagraj",price:200,desc:"Game"},
+    {location:"Barabanki",price:150,desc:"Fodd"},
+    {location:"lucknow",price:50,desc:"Food"},
+    {location:"Kanpur",price:100,desc:"Movie"},
+    {location:"Prayagraj",price:200,desc:"Game"},
+    {location:"Barabanki",price:150,desc:"Fodd"},
+    {location:"lucknow",price:50,desc:"Food"},
+    {location:"Kanpur",price:100,desc:"Movie"},
+    {location:"Prayagraj",price:200,desc:"Game"},
+    {location:"Barabanki",price:150,desc:"Fodd"}
+  ]
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <h2> Expense Item</h2>
+      {expenses.map((ele) => {
+        return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc}></ExpenseItem>
+      })}
     </div>
   );
 }
