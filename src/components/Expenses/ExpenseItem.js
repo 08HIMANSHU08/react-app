b2a3b052c785c1101483016fd03c2374ee823dac
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 20:31:33 +0530

    expense item

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import Card from '../UI/Card'
+
+function ExpenseItem(props) {
+    // console.log(props)
+    return (
+            <Card className='expense-item'> 
+            <ExpenseDate date ={props.date}></ExpenseDate>
+            <ExpenseDetails location={props.location} desc={props.desc} price={props.price}></ExpenseDetails> 
+            </Card>  
+    )
+}
+
+export default ExpenseItem;
