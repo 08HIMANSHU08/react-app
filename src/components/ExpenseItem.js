b28ba46d76a43b12f7c12f908e11ded6f00ebbd0
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 15:25:39 +0530

    expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
-function ExpenseItem(){
+
+import './ExpenseItem.css';
+
+function ExpenseItem() {
     return (
-        <div>
-        <h2> Expense Item</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+        <div className='expense-item'>
+            <div>
+                <h2> Expense Item</h2>
+                <p>Lucknow</p>
+            </div>
+            <div className='expense-item__description'>
+                <p className='expense-item__price'>Food Rs 10</p>
+                <p className='expense-item__price'>Petrol Rs 100</p>
+                <p className='expense-item__price'>Movies Rs 200</p>
+            </div>
         </div>
-        )
+    )
 }
 
 export default ExpenseItem;
+
+ 
