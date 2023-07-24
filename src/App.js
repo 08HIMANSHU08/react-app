0c347bc77b56c3bc60b96fe2be34d4ea795fcb34
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 18:00:04 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,16 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses = [
-    {location:"lucknow",price:50,desc:"Food"},
-    {location:"Kanpur",price:100,desc:"Movie"},
-    {location:"Prayagraj",price:200,desc:"Game"},
-    {location:"Barabanki",price:150,desc:"Fodd"},
-    {location:"lucknow",price:50,desc:"Food"},
-    {location:"Kanpur",price:100,desc:"Movie"},
-    {location:"Prayagraj",price:200,desc:"Game"},
-    {location:"Barabanki",price:150,desc:"Fodd"},
-    {location:"lucknow",price:50,desc:"Food"},
-    {location:"Kanpur",price:100,desc:"Movie"},
-    {location:"Prayagraj",price:200,desc:"Game"},
-    {location:"Barabanki",price:150,desc:"Fodd"}
+    {location:"Lucknow",price:50,desc:"Food",date:new Date(2023,2,12)},
+    {location:"Kanpur",price:100,desc:"Movie",date:new Date(2022,4,9)},
+    {location:"Prayagraj",price:200,desc:"Game",date:new Date(2022,8,8)},
+    {location:"Barabanki",price:150,desc:"Food",date:new Date(2022,10,3)}
   ]
   return (
     <div>
       <h2> Expense Item</h2>
       {expenses.map((ele) => {
-        return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc}></ExpenseItem>
+        return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc} date={ele.date}></ExpenseItem>
       })}
     </div>
   );
