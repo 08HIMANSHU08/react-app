1252b30ed08083e6df4dc7c4f23c7d7ebe3616e8
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-24 17:08:00 +0530

    EpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,32 @@
 
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    console.log(props)
     return (
-        <div className='expense-item'>
-            <div>
-                <h2> Expense Item</h2>
-                <p>Lucknow</p>
-            </div>
-            <div className='expense-item__description'>
-                <p className='expense-item__price'>Food Rs 10</p>
-                <p className='expense-item__price'>Petrol Rs 100</p>
-                <p className='expense-item__price'>Movies Rs 200</p>
-            </div>
+        <div>
+            <div className='expense-item'> 
+                <div className='expense-item__description'>
+                    <p>{props.location}</p>
+                    <p>{props.desc}</p>
+                    <div className='expense-item__price'> Rs {props.price}</div>
+                </div>  
+            </div>  
         </div>
     )
 }
 
 export default ExpenseItem;
 
- 
+/* <div className='expense-item'> 
+<div className='expense-item__description'>
+    <p>Kanpur</p>
+    <div className='expense-item__price'>Movies Rs 200</div>
+</div>   
+</div>
+<div className='expense-item'> 
+<div className='expense-item__description'>
+    <p>Prayagraj</p>
+    <div className='expense-item__price'>Petrol Rs 100</div>
+</div>
+</div>  */
