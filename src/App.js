84d076fc7aac2d63748ecd4afebb1785d9164ee0
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-25 12:49:12 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import React from 'react';
 
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import './components/Expenses/Expenses.css';
+import NewExpense from './components/NewExpense/NewExpense'
 import Card from './components/UI/Card';
 
-function App() {
+const App = ()=> {
   const expenses = [
     {location:"Lucknow",price:50,desc:"Food",date:new Date(2023,2,12)},
-    {location:"Kanpur",price:100,desc:"Movie",date:new Date(2022,4,9)},
+    {location:"Kanpur",price:300,desc:"Movie",date:new Date(2022,4,9)},
     {location:"Prayagraj",price:200,desc:"Game",date:new Date(2022,8,8)},
     {location:"Barabanki",price:150,desc:"Food",date:new Date(2022,10,3)}
   ]
   return (
     <Card className='expense'>
       <h2> Expense Item</h2>
+      <NewExpense></NewExpense>
       {expenses.map((ele) => {
         return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc} date={ele.date}></ExpenseItem>
       })}
