ba43b6a38b312afc7d6ac43e5beb9e3ac4e842f0
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-25 13:37:50 +0530

    epense form with single state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -9,22 +9,63 @@ const ExpenseForm = () => {
     const [enteredLocation, setEnteredLocation] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
     const [enteredPrice, setEnteredPrice] = useState('');
-    
+    // const [userInput,setUserInput] = useState({
+    //     enteredDesc:'',
+    //     enteredLocation:'',
+    //     enteredDate:'',
+    //     enteredPrice:''
+    // })
     const locationChangeHandler = (event)=>{
         setEnteredLocation(event.target.value);
-       
+        // setUserInput({
+        //     ...userInput,
+        //     enteredLocation:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //     return {
+        //         ...prevState,
+        //         enteredLocation:event.target.value,
+        //     }
+        // })
     }
     const dateChangeHandler = (event) =>{
         setEnteredDate(event.target.value);
-        
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate:event.target.value,
+        // })
+    //     setUserInput((prevState)=>{
+    //         return {
+    //             ...prevState,
+    //             enteredDate:event.target.value,
+    //         }
+    //     })
     };
     const descChangeHandler = (event) =>{
         setEnteredDesc(event.target.value)
-        
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDesc:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //     return {
+        //         ...prevState,
+        //         enteredDesc:event.target.value,
+        //     }
+        // })
     };
     const priceChangeHandler = (event) =>{
         setEnteredPrice(event.target.value)
-       
+        // setUserInput({
+        //     ...userInput,
+        //     enteredPrice:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //     return {
+        //         ...prevState,
+        //         enteredPrice:event.target.value,
+        //     }
+        // })
     };
 
     
