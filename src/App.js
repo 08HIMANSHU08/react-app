fd9faa01162e8b534d1c6d40800c89a5d4893e8f
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-27 13:07:21 +0530

    app.js with key

--- src/App.js
+++ src/App.js
@@ -7,30 +7,34 @@ import NewExpense from './components/NewExpense/NewExpense';
 import ExpenseFilter from './components/Expenses/ExpenseFilter'
 import Card from './components/UI/Card';
 
+let DummyExpenses = [
+  {id:1,location:"Lucknow",price:50,desc:"Food",date:new Date(2020,2,12)},
+  {id:2,location:"Kanpur",price:300,desc:"Movie",date:new Date(2022,4,9)},
+  {id:3,location:"Prayagraj",price:200,desc:"Game",date:new Date(2021,8,8)},
+  {id:4,location:"Barabanki",price:150,desc:"Food",date:new Date(2019,10,3)}
+]
+
 const App = ()=> {
 
-  let expenses = [
-    {id:1,location:"Lucknow",price:50,desc:"Food",date:new Date(2020,2,12)},
-    {id:2,location:"Kanpur",price:300,desc:"Movie",date:new Date(2022,4,9)},
-    {id:3,location:"Prayagraj",price:200,desc:"Game",date:new Date(2021,8,8)},
-    {id:4,location:"Barabanki",price:150,desc:"Food",date:new Date(2019,10,3)}
-  ]
+  const [expenses,setExpenses] = useState(DummyExpenses);
+
+  const [filteredYear, setFilteredYear] = useState('2020')
 
   const filterChangeHandler = selectedYear =>{
     setFilteredYear(selectedYear);
   }
-  const [filteredYear, setFilteredYear] = useState('2020')
+  
   const addExpenseHandler = (expense) =>{
-      console.log("hello");
+      setExpenses([expense,...expenses])
   }
 
   return (
     <Card className='expense'>
       <h2> Expense Item</h2>
-      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpenseFilter>
       {expenses.map((ele) => {
-        return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc} date={ele.date}></ExpenseItem>
+        return <ExpenseItem key={ele.id} location={ele.location} price={ele.price}desc={ele.desc} date={ele.date}></ExpenseItem>
       })}
     </Card>
   );
