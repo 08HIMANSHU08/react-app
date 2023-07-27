2c3f2b28e149423434e823594694d564bd96a995
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-27 11:17:03 +0530

    app.js with input data from new edpense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React,{useState} from 'react';
 
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import './components/Expenses/Expenses.css';
@@ -7,16 +7,21 @@ import NewExpense from './components/NewExpense/NewExpense'
 import Card from './components/UI/Card';
 
 const App = ()=> {
-  const expenses = [
-    {location:"Lucknow",price:50,desc:"Food",date:new Date(2023,2,12)},
-    {location:"Kanpur",price:300,desc:"Movie",date:new Date(2022,4,9)},
-    {location:"Prayagraj",price:200,desc:"Game",date:new Date(2022,8,8)},
-    {location:"Barabanki",price:150,desc:"Food",date:new Date(2022,10,3)}
+  let expenses = [
+    {id:1,location:"Lucknow",price:50,desc:"Food",date:new Date(2023,2,12)},
+    {id:2,location:"Kanpur",price:300,desc:"Movie",date:new Date(2022,4,9)},
+    {id:3,location:"Prayagraj",price:200,desc:"Game",date:new Date(2022,8,8)},
+    {id:4,location:"Barabanki",price:150,desc:"Food",date:new Date(2022,10,3)}
   ]
+
+  const addExpenseHandler = (expense) =>{
+      console.log("hello");
+  }
+
   return (
     <Card className='expense'>
       <h2> Expense Item</h2>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       {expenses.map((ele) => {
         return <ExpenseItem location={ele.location} price={ele.price}desc={ele.desc} date={ele.date}></ExpenseItem>
       })}
