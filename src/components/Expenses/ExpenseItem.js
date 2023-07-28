95b4e550d37220fa67c92ea475158496db52251b
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-28 09:57:59 +0530

    expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
 
-import React,{useState} from 'react';
+import React from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
@@ -8,25 +8,14 @@ import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
 
-    const [desc,setDesc] = useState(props.desc);
-
-    const [price, setPrice] = useState(props.price);
-    
-    const priceClickHandler = () =>{
-        setPrice(100);
-    }
-
-    const clickHandler = () => {
-        setDesc('UpdatedFood');
-    }
 
     return (
-            <Card className='expense-item'> 
-            <ExpenseDate date ={props.date}></ExpenseDate>
-            <ExpenseDetails location={props.location} desc={desc} price={price}></ExpenseDetails> 
-            <button onClick={clickHandler}>Change Desc</button>
-            <button onClick={priceClickHandler}>Change Price</button>
-            </Card>  
+            <li>
+                <Card className='expense-item'> 
+                <ExpenseDate date ={props.date}></ExpenseDate>
+                <ExpenseDetails location={props.location} desc={props.desc} price={props.price}></ExpenseDetails> 
+                </Card> 
+            </li> 
     )
 }
 
