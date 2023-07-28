d9e72c24531550e5ddc909f92d6bb5cc594e4ec9
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-07-28 09:58:29 +0530

    expense date and details

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './ExpenseItem.css';
 
 
-function ExpenseDetails(props) {
+const ExpenseDetails = (props) => {
     return (
         <div className='expense-item__description'>
             <h2>{props.location}</h2>
